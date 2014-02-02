f7ec00e5ea0034ae662fada558c8be16144064da
Philipp Meisberger <team@pm-codeworks.de>
2014-02-02 19:16:51 +0100

    Added script for man page gzipping.

---
+++ ManPage/gzipMan.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+gzip -c pamfingerprint-conf.1 > ../src/usr/share/man/man1/pamfingerprint-conf.1.gz
--- src/install.sh
+++ src/install.sh
@@ -6,7 +6,8 @@ cp etc/pam_fingerprint.conf /etc/
 ## chmod 600 /etc/pam_fingerprint.conf
 
 cp lib/security/pam_fingerprint.py /lib/security/
-cp usr/bin/pam_fingerprint-conf /usr/bin/
+# TODO: better ln -s usr/bin/pam_fingerprint-conf /usr/bin/pamfingerprint-conf
+cp usr/bin/pamfingerprint-conf /usr/bin/
 cp -r usr/lib/* /usr/lib/
 
 cp var/log/pam_fingerprint.log /var/log/
