7230477802c7c2debc54b0453c75230a05a6da1b
Bastian Raschke <bastian.raschke@posteo.de>
2014-02-05 21:46:53 +0100

    Fixed pam-auth-config; Improved postinst

--- src/install.sh
+++ src/install.sh
@@ -3,12 +3,8 @@
 find . -type f -name *.pyc -exec rm {} \;
 
 cp etc/pamfingerprint.conf /etc/
-## TODO
-## chmod 600 /etc/pamfingerprint.conf
 
 cp lib/security/pam_fingerprint.py /lib/security/
-cp -r usr/lib/* /usr/lib/
+cp -r usr/* /usr/
 
 cp var/log/pamfingerprint.log /var/log/
-## TODO
-chown 1000:1000 /var/log/pamfingerprint.log
