ce8070dbe6925a53fc0cd04daf378e9e1f363aaf
Philipp Meisberger <team@pm-codeworks.de>
2014-02-08 11:38:10 +0100

    Changed permission rights in install.sh Added root permission check to pamfingerprint-conf.

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@
 find ./src/ -type f -name *.pyc -exec rm {} \;
 
 cp ./src/etc/pamfingerprint.conf /etc/
+chmod 744 /etc/pamfingerprint.conf
 
 cp ./src/lib/security/pam_fingerprint.py /lib/security/
 cp -r ./src/usr/* /usr/
+chmod 700 /usr/bin/pamfingerprint-conf
 
 cp ./src/var/log/pamfingerprint.log /var/log/
-chown 1000:1000 /var/log/pamfingerprint.log
+#chmod 1000:1000 /var/log/pamfingerprint.log
