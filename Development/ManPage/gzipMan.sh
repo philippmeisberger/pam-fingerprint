9b598a63f346d6cfb1ad97d329902884ba2a4cd2
Philipp Meisberger <team@pm-codeworks.de>
2014-02-10 16:16:28 +0100

    Added man page for check program. Excluded PyFingerprint copying from install because of inconsistent dev state.

--- Development/ManPage/gzipMan.sh
+++ Development/ManPage/gzipMan.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 gzip -c pamfingerprint-conf.1 > ../../src/usr/share/man/man1/pamfingerprint-conf.1.gz
+gzip -c pamfingerprint-check.1 > ../../src/usr/share/man/man1/pamfingerprint-check.1.gz
--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ cp -r ./src/etc/* /etc/
 chmod 644 /etc/pamfingerprint.conf
 
 cp ./src/lib/security/pam_fingerprint.py /lib/security/
-cp -r ./src/usr/* /usr/
+#cp -r ./src/usr/* /usr/
+cp -r ./src/usr/lib/pamfingerprint /usr/lib/
 #chmod 700 /usr/bin/pamfingerprint-conf
 
 cp ./src/var/log/pamfingerprint.log /var/log/
