7b7af0a31c4a6e9ab4b7f0e746a4027463389409
Philipp Meisberger <team@pm-codeworks.de>
2014-02-19 16:12:09 +0100

    Changes in man pages. Added new general man page.

--- Development/ManPage/gzipMan.sh
+++ Development/ManPage/gzipMan.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+gzip -c pamfingerprint.1 > ../../src/usr/share/man/man1/pamfingerprint.1.gz
 gzip -c pamfingerprint-conf.1 > ../../src/usr/share/man/man1/pamfingerprint-conf.1.gz
 gzip -c pamfingerprint-check.1 > ../../src/usr/share/man/man1/pamfingerprint-check.1.gz
