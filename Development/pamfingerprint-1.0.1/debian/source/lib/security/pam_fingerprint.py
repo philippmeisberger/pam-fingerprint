ffb48b8de94dd05d0f1fe70ecbfda4ceb9ecac56
Philipp Meisberger <team@pm-codeworks.de>
2014-03-30 12:45:56 +0200

    Added files for *.dsc package.

---
+++ Development/pamfingerprint-1.0.1/debian/source/lib/security/pam_fingerprint.py
@@ -0,0 +1,166 @@
+"""
+"" pamfingerprint
+"" PAM implementation.
+""
+"" Copyright 2014 Philipp Meisberger, Bastian Raschke.
+"" All rights reserved. 
+"""
+
+import sys 
+sys.path.append('/usr/lib')
+
+from pamfingerprint.version import VERSION
+from pamfingerprint.Config import *
+
+from PyFingerprint.PyFingerprint import *
+
+import hashlib
+import logging
+
+## Configures logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+## Creates and adds a file handler to logger
+fileHandler = logging.FileHandler('/var/log/pamfingerprint.log')
+fileHandler.setLevel(logging.INFO)
+fileHandler.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] %(message)s'))
+logger.addHandler(fileHandler)
+
+
+"""
+"" PAM service function for user authentication.
+""
+"" @param pamh
+"" @param flags
+"" @param argv
+"" @return integer
+"""
+def pam_sm_authenticate(pamh, flags, argv):
+
+    ## Tries to get user which is asking for permission
+    try:
+        userName = pamh.ruser
+
+        ## Fallback
+        if ( userName == None ):
+            userName = pamh.get_user()            
+
+        ## Be sure the user is set 
+        if ( userName == None ):
+            raise Exception('The user is not known!')
+
+    except:
+        e = sys.exc_info()[1]
+        logger.error(e.message, exc_info=True)
+        return pamh.PAM_USER_UNKNOWN
+
+    ## Tries to init Config
+    try:
+        config = Config('/etc/pamfingerprint.conf')
+
+    except:
+        e = sys.exc_info()[1]
+        logger.error(e.message, exc_info=True)
+        return pamh.PAM_IGNORE
+
+    logger.info('The user "' + userName + '" is asking for permission for service "' + str(pamh.service) + '".')
+
+    ## Checks if the the user was added in configuration
+    if ( config.itemExists('Users', userName) == False ):
+        logger.error('The user was not added!')
+        return pamh.PAM_IGNORE
+
+    ## Tries to get user information (template position, fingerprint hash)
+    try:
+        userData = config.readList('Users', userName)
+        
+        ## Validates user information
+        if ( len(userData) != 2 ):
+            raise Exception('The user information of "' + userName + '" is invalid!')
+
+        expectedPositionNumber = int(userData[0])
+        expectedFingerprintHash = userData[1]
+
+    except:
+        e = sys.exc_info()[1]
+        logger.error(e.message, exc_info=False)
+        return pamh.PAM_AUTH_ERR
+
+    ## Gets sensor connection values
+    port = config.readString('PyFingerprint', 'port')
+    baudRate = config.readInteger('PyFingerprint', 'baudRate')
+    address = config.readHex('PyFingerprint', 'address')
+    password = config.readHex('PyFingerprint', 'password')
+
+    ## Tries to init PyFingerprint
+    try:
+        fingerprint = PyFingerprint(port, baudRate, address, password)
+
+        if ( fingerprint.verifyPassword() == False ):
+            raise ValueError('The given fingerprint sensor password is wrong!')
+
+    except:
+        logger.error('The fingerprint sensor could not be initialized!', exc_info=True)
+        pamh.conversation(pamh.Message(pamh.PAM_TEXT_INFO, 'pamfingerprint ' + VERSION + ': Sensor initialization failed!'))
+        return pamh.PAM_IGNORE
+
+    msg = pamh.Message(pamh.PAM_TEXT_INFO, 'pamfingerprint ' + VERSION + ': Waiting for finger...')
+    pamh.conversation(msg)
+
+    ## Tries to read fingerprint
+    try:
+        while ( fingerprint.readImage() == False ):
+            pass
+
+        fingerprint.convertImage(0x01)
+
+        ## Gets position of template
+        result = fingerprint.searchTemplate()
+        positionNumber = result[0]
+
+        ## Checks if the template position is invalid
+        if ( positionNumber == -1 ):
+            raise Exception('No match found!')
+
+        ## Checks if the template position is correct
+        if ( positionNumber != expectedPositionNumber ):
+            raise Exception('The template position of the found match is not equal to the stored one!')
+
+        ## Gets characteristics
+        fingerprint.loadTemplate(positionNumber, 0x01)
+        characterics = fingerprint.downloadCharacteristics(0x01)
+
+        ## Calculates hash of template
+        fingerprintHash = hashlib.sha256(str(characterics)).hexdigest()
+
+        ## Checks if the calculated hash is equal to expected hash from user
+        if ( fingerprintHash == expectedFingerprintHash ):
+            logger.info('Access granted!')
+            pamh.conversation(pamh.Message(pamh.PAM_TEXT_INFO, 'pamfingerprint ' + VERSION + ': Access granted!'))
+            return pamh.PAM_SUCCESS
+        else:
+            logger.info('The found match is not assigned to user!')
+            pamh.conversation(pamh.Message(pamh.PAM_TEXT_INFO, 'pamfingerprint ' + VERSION + ': Access denied!'))
+            return pamh.PAM_AUTH_ERR
+
+    except:
+        e = sys.exc_info()[1]
+        logger.error('Fingerprint read failed!', exc_info=True)
+        pamh.conversation(pamh.Message(pamh.PAM_TEXT_INFO, 'pamfingerprint ' + VERSION + ': Access denied!'))
+        return pamh.PAM_AUTH_ERR
+
+    ## Denies for default
+    return pamh.PAM_AUTH_ERR
+
+"""
+"" PAM service function to alter credentials.
+""
+"" @param pamh
+"" @param flags
+"" @param argv
+"" @return integer
+"""
+def pam_sm_setcred(pamh, flags, argv):
+
+    return pamh.PAM_SUCCESS
---
+++ Development/pamfingerprint-1.0.1/debian/source/usr/lib/pamfingerprint/Config.py
@@ -0,0 +1,214 @@
+﻿"""
+"" pamfingerprint
+"" Configuration file management.
+"" 
+"" Copyright 2014 Philipp Meisberger, Bastian Raschke.
+"" All rights reserved.
+"""
+
+## Documentation: @see http://docs.python.org/3/library/configparser.html
+import ConfigParser
+import os
+
+
+class Config(object):
+
+    """
+    "" The config file path.
+    "" @var string
+    """
+    __configFile = None
+
+    """
+    "" Flag that indicates config file will be not modified.
+    "" @var boolean
+    """
+    __readOnly = False
+
+    """
+    "" The ConfigParser instance.
+    "" @var ConfigParser
+    """
+    __configParser = None
+    
+    """
+    "" Constructor
+    ""
+    "" @param string configFile
+    "" @param boolean readOnly
+    "" @return void
+    """
+    def __init__(self, configFile, readOnly = False):
+
+        # Checks if path/file is readable
+        if ( os.access(configFile, os.R_OK) == False ):
+            raise Exception('The configuration file "' + configFile + '" is not readable!')
+
+        self.__configFile = configFile
+        self.__readOnly = readOnly
+
+        self.__configParser = ConfigParser.ConfigParser()
+        self.__configParser.read(configFile)
+
+    """
+    "" Destructor
+    ""
+    "" @return void
+    """
+    def __del__(self):
+
+        self.save()
+
+    """
+    "" Writes modifications to config file.
+    ""
+    "" @return boolean
+    """
+    def save(self):
+
+        if ( self.__readOnly == True ):
+            return False
+
+        # Checks if path/file is writable
+        if ( os.access(self.__configFile, os.W_OK) == True ):
+
+            f = open(self.__configFile, 'w')
+            self.__configParser.write(f)
+            f.close()
+
+            return True
+
+        return False
+
+    """
+    "" Reads a string value.
+    ""
+    "" @param string section
+    "" @param string name
+    "" @return string
+    """
+    def readString(self, section, name):
+
+        return self.__configParser.get(section, name)
+
+    """
+    "" Writes a string value.
+    ""
+    "" @param string section
+    "" @param string name
+    "" @param string value
+    "" @return void
+    """
+    def writeString(self, section, name, value):
+
+        self.__configParser.set(section, name, value)
+
+    """
+    "" Reads a boolean value.
+    ""
+    "" @param string section
+    "" @param string name
+    "" @return boolean
+    """
+    def readBoolean(self, section, name):
+
+        return self.__configParser.getboolean(section, name)
+
+    """
+    "" Reads a decimal integer value.
+    ""
+    "" @param string section
+    "" @param string name
+    "" @return integer
+    """
+    def readInteger(self, section, name):
+
+        ## Casts to integer (base 10)
+        return int(self.readString(section, name), 10)
+
+    """
+    "" Reads a hexadecimal integer value.
+    ""
+    "" @param string section
+    "" @param string name
+    "" @return integer
+    """
+    def readHex(self, section, name):
+
+        ## Casts to integer (base 16)
+        return int(self.readString(section, name), 16)
+
+    """
+    "" Reads a list.
+    ""
+    "" @param string section
+    "" @param string name
+    "" @return list
+    """
+    def readList(self, section, name):
+
+        unpackedList = self.readString(section, name)
+        return unpackedList.split(',')
+
+    """
+    "" Writes a list.
+    ""
+    "" @param string section
+    "" @param string name
+    "" @param list value
+    "" @return void
+    """
+    def writeList(self, section, name, value):
+
+        delimiter = ','
+        self.__configParser.set(section, name, delimiter.join(value))
+
+    """
+    "" Removes a value.
+    ""
+    "" @param string section
+    "" @param string name
+    "" @return boolean
+    """
+    def remove(self, section, name):
+
+        return self.__configParser.remove_option(section, name)
+
+    """
+    "" Checks if a given section exists.
+    ""
+    "" @param string section
+    "" @return boolean
+    """
+    def sectionExists(self, section):
+
+        return self.__configParser.has_section(section)
+
+    """
+    "" Checks if an item in a given section exists.
+    ""
+    "" @param string section
+    "" @param string name
+    "" @return boolean
+    """
+    def itemExists(self, section, name):
+
+        return self.__configParser.has_option(section, name)
+
+    """
+    "" Returns all sections as a list.
+    ""
+    "" @return list
+    """
+    def getSections(self):
+
+        return self.__configParser.sections()
+
+    """
+    "" Returns all items of a sections as a list.
+    ""
+    "" @return list
+    """
+    def getItems(self, section):
+
+        return self.__configParser.items(section)
