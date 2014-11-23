5ff3657a1de6d9d0d3c5fa91fbf2249ed78194d6
Philipp Meisberger <team@pm-codeworks.de>
2014-11-23 15:24:26 +0100

    Total project restructure: Migrated project to Debian source package; Removed old *.deb files and other unused stuff
    --HG--
    rename : Development/ManPage/ManPageMarkup => Development/ManPageMarkup
    rename : src/usr/share/doc/pamfingerprint/authors => debian/authors
    rename : src/usr/share/doc/pamfingerprint/copyright => debian/copyright
    rename : Development/ManPage/pamfingerprint-check.1 => debian/docs/pamfingerprint-check.1
    rename : Development/ManPage/pamfingerprint-conf.1 => debian/docs/pamfingerprint-conf.1
    rename : Development/ManPage/pamfingerprint.1 => debian/docs/pamfingerprint.1
    rename : src/DEBIAN/postinst => debian/postinst
    rename : src/DEBIAN/prerm => debian/prerm

--- Development/ManPage/gzipMan.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-gzip -c pamfingerprint.1 > ../../src/usr/share/man/man1/pamfingerprint.1.gz
-gzip -c pamfingerprint-conf.1 > ../../src/usr/share/man/man1/pamfingerprint-conf.1.gz
-gzip -c pamfingerprint-check.1 > ../../src/usr/share/man/man1/pamfingerprint-check.1.gz
